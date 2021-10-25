d49439cca4c5be70c78cbce33137dddec639f0b4
Víctor García <vgpastor@ingenierosweb.co>
2021-10-25 23:01:53 +0200

    Add CORS to allow petitions between front and back

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
